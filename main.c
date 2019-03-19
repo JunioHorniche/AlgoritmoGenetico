d145cf58655b8580ce2d7060bf16ac552076a04c
juniohorniche <juniohorniche@gmail.com>
2019-03-19 00:08:28 -0300

    Declaração das constantes

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-#define PESO 10
+#define crossoverRate 50 //  1 a 99
+#define mutationRate 20
+#define killingRate 100
+
+#define numberOfGenerations 1000
+#define numberOfIndividuals 10
+#define numberOfGenes 5
+#define numberOfDeaths 2
 
 int int main(int argc, char const *argv[])
 {
-	printf("Hello Word!!!\n");
-	int a, b;
+	printf("Hello Word !!!\n");
 	return 0;
 }
